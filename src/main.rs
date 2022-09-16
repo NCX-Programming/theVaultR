b4829e8b991ba65379182648c0743d8fc74d7786
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-09-16 15:11:45 +0000

    Fixed HelloWorld and main menu

--- src/main.rs
+++ src/main.rs
@@ -30,17 +30,18 @@ fn main() {
     for c in stdin.keys() {
         write!(stdout,
                "{}{}",
-               termion::cursor::Goto(1, 1),
+               termion::cursor::Goto(1, 4),
                termion::clear::CurrentLine)
                 .unwrap();
 
         match c.unwrap() {
             Key::Char('q') => break,
-            Key::Char('1') => helloworld::hello_world(),
-            Key::Backspace => println!("×"),
+            Key::Char('1') => {
+                helloworld::hello_world();
+                break;
+            },
             _ => {}
         }
         stdout.flush().unwrap();
-        break;
     }
 }
